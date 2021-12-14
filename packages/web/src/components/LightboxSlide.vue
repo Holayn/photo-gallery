61550f9fba37754d800003033170e442bb85212f
Holayn <kwwong1597@gmail.com>
2021-12-13 22:33:01 -0500

    fix(web): zoom fixes
    - image needs to be first child of swiper-slide in order for zoom to work
    - loading image size fixes

--- packages/web/src/components/LightboxSlide.vue
+++ packages/web/src/components/LightboxSlide.vue
@@ -1,22 +1,19 @@
 <template>
-  <div style="display: flex; align-items: center; justify-content: center; height: 100vh; width: 100vw;">
-    <div v-if="loading" style="position: absolute; width: 100%;">
-      <div style="position: relative; height: 100%; width: 100%;">
-        <img style="width: 100%; filter: blur(3px);" :src="getUrl(photo.output.small.path)">
-        <div style="position: absolute; display: flex; align-items: center; justify-content: center; top: 0; left: 0; width: 100%; height: 100%;">
-          <Loading></Loading>
-        </div>
+  <div v-if="loading" style="width: 100vw; height: 100vh;">
+    <img v-show="false" @load="loaded" :src="getUrl(photo.output.large.path)" style="max-width: 100%; max-height: 100vh;">
+    <div style="position: relative; display: flex; align-items: center; justify-content: center; height: 100%;">
+      <img style="width: 100%; height: 100%; object-fit: contain; filter: blur(3px);" :src="getUrl(photo.output.small.path)">
+      <div style="position: absolute; display: flex; align-items: center; justify-content: center; top: 0; left: 0; width: 100%; height: 100%;">
+        <Loading></Loading>
       </div>
-    </div>
-    <div style="display: flex; align-items: center; justify-content: center; width: 100%; height: 100%;">
-      <div v-if="photo.isVideo" style="display: flex; align-items: center; justify-content: center; height: 100%;">
-        <video :id="'video-' + index" class="video-js" controls preload="auto" :poster="getUrl(photo.output.small.path)" style="height: 100%; width: 100%; max-height: 75vh;">
-          <source :src="getUrl(photo.output.download.path)" type="video/mp4"/>
-        </video>
-      </div>
-      <img v-else @load="loaded" :src="getUrl(photo.output.large.path)" style="max-width: 100%; max-height: 100vh;">
     </div>
   </div>
+  <div v-else-if="photo.isVideo" style="display: flex; align-items: center; justify-content: center; height: 100%;">
+    <video :id="'video-' + index" class="video-js" controls preload="auto" :poster="getUrl(photo.output.small.path)" style="height: 100vw; width: 100vw; max-height: 75vh;">
+      <source :src="getUrl(photo.output.download.path)" type="video/mp4"/>
+    </video>
+  </div>
+  <img v-else :src="getUrl(photo.output.large.path)" style="max-width: 100%; max-height: 100vh;">
 </template>
 
 <script>
