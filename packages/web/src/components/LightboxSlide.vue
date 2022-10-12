9cf2cb6afd25e45735ace73a6302d62b98248be6
kai <kwwong1597@gmail.com>
2022-10-11 22:08:26 -0400

    fix(web): bad video lightbox display.

--- packages/web/src/components/LightboxSlide.vue
+++ packages/web/src/components/LightboxSlide.vue
@@ -1,19 +1,19 @@
 <template>
-  <div v-if="photo && loading" style="width: 100vw; height: 100vh;">
-    <div style="position: relative; display: flex; align-items: center; justify-content: center; height: 100%;">
+  <div v-if="photo && !photo.metadata.video" style="width: 100vw; height: 100vh;">
+    <div v-if="loading" style="position: relative; display: flex; align-items: center; justify-content: center; height: 100%;">
       <img style="width: 100%; height: 100%; object-fit: contain; filter: blur(3px);" :src="toPhotoUrl(photo, getGalleryPhotoSize())">
       <div style="position: absolute; display: flex; align-items: center; justify-content: center; top: 0; left: 0; width: 100%; height: 100%;">
         <Loading></Loading>
       </div>
+    </div>
+    <div v-show="!loading" style="display: flex; justify-content: center; width: 100vw; height: 100vh;">
+      <img @load="loaded" :src="toPhotoUrl(photo, PHOTO_SIZES.LARGE)" style="max-width: 100%; max-height: 100%; object-fit: contain;">
     </div>
   </div>
   <div v-else-if="photo.metadata.video" style="display: flex; align-items: center; justify-content: center; height: 100%;">
     <video :id="'video-' + index" class="video-js" controls preload="auto" :poster="toPhotoUrl(photo, getGalleryPhotoSize())" style="height: 100vw; width: 100vw; max-height: 75vh;">
       <source :src="toPhotoUrl(photo, PHOTO_SIZES.LARGE)" type="video/mp4"/>
     </video>
-  </div>
-  <div v-show="!loading" style="display: flex; justify-content: center; width: 100vw; height: 100vh;">
-    <img @load="loaded" :src="toPhotoUrl(photo, PHOTO_SIZES.LARGE)" style="max-width: 100%; max-height: 100%; object-fit: contain;">
   </div>
 </template>
 
