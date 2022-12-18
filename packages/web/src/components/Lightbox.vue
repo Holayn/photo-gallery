044bab24a262bd36124af981415c672e56775af0
Holayn <kwwong1597@gmail.com>
2022-12-17 23:56:12 -0500

    feat: ability to view photo metadata.

--- packages/web/src/components/Lightbox.vue
+++ packages/web/src/components/Lightbox.vue
@@ -4,7 +4,10 @@
       <div ref="download" @click="triggerDownloadTooltip()" style="margin: 1rem; z-index: 99; cursor: pointer;">
         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#ffffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M3 15v4c0 1.1.9 2 2 2h14a2 2 0 0 0 2-2v-4M17 9l-5 5-5-5M12 12.8V2.5"/></svg>
       </div>
-      <div @click="close()" style="margin: 1rem; z-index: 99; cursor: pointer;">
+      <div class="m-4 cursor-pointer" @click="showMetadata = true">
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#ffffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><circle cx="12" cy="12" r="10"></circle><line x1="12" y1="8" x2="12" y2="12"></line><line x1="12" y1="16" x2="12.01" y2="16"></line></svg>
+      </div>
+      <div class="m-4 cursor-pointer" @click="close()">
         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#ffffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg>
       </div>
     </div>
@@ -31,6 +34,15 @@
         ></lightbox-slide>
       </swiper-slide>
     </swiper>
+
+    <Modal v-if="showMetadata" @close="(showMetadata = false)">
+      <div class="grid grid-cols-3 gap-x-6 gap-y-2">
+        <div v-for="meta in currentPhotoMetadata" :key="meta.label">
+          <div class="text-sm">{{ meta.label }}</div>
+          <div>{{ meta.value }}</div>
+        </div>
+      </div>
+    </Modal>
   </div>
 </template>
 
@@ -43,11 +55,13 @@ import 'swiper/modules/navigation/navigation.scss'; // Navigation module
 import 'swiper/modules/pagination/pagination.scss'; // Pagination module
 
 import LightboxSlide from './LightboxSlide.vue';
+import Modal from './Modal.vue';
 
 export default {
   name: 'Lightbox',
   components: {
     LightboxSlide,
+    Modal,
     Swiper,
     SwiperSlide,
   },
@@ -64,12 +78,16 @@ export default {
     return {
       swiper: null,
       tippy: null,
+
+      showMetadata: false,
     }
   },
   computed: {
-    initialSlide() {
-      return this.$store.state.lightbox.photoIndex;
-    },
+    currentPhotoMetadata() {
+      const metadata = this.$store.state.photos[this.$store.state.lightbox.photoIndex].metadata;
+      metadata.date = new Date(metadata.date);
+      return ['date', 'fileName', 'fileSize', 'width', 'height', 'orientation'].map(field => ({ label: field, value: metadata[field] }));
+    }
   },
   mounted() {
     this.tippy = window.tippy(this.$refs.download, {
