3bf22779650c53219bd7ea419e629e3be6e2cd0f
Holayn <kwwong1597@gmail.com>
2022-12-31 22:08:20 -0500

    feat(server): do not store metadata.

--- packages/server/model/file.js
+++ packages/server/model/file.js
@@ -1,23 +1,19 @@
 const DB = require('../services/db');
 
-const FileMetadata = require('../metadata/file-metadata');
-
-DB.exec('CREATE TABLE IF NOT EXISTS file (id INTEGER PRIMARY KEY, timestamp_added INTEGER, date INTEGER, metadata TEXT, source_id INTEGER, source_file_id TEXT, FOREIGN KEY(source_id) REFERENCES source(id))');
+DB.exec('CREATE TABLE IF NOT EXISTS file (id INTEGER PRIMARY KEY, timestamp_added INTEGER, date INTEGER, source_id INTEGER, source_file_id TEXT, FOREIGN KEY(source_id) REFERENCES source(id))');
 
 class File {
   constructor({ id, date, metadata, sourceId, sourceFileId }) {
     this.id = id;
     this.date = date;
-    this.metadata = new FileMetadata(metadata);
     this.sourceId = sourceId;
     this.sourceFileId = sourceFileId;
   }
 
   static insert(file) {
-    const { lastInsertRowid } = DB.prepare(`INSERT INTO file (timestamp_added, date, metadata, source_id, source_file_id) VALUES (@timestampAdded, @date, @metadata, @sourceId, @sourceFileId)`)
+    const { lastInsertRowid } = DB.prepare(`INSERT INTO file (timestamp_added, date, source_id, source_file_id) VALUES (@timestampAdded, @date, @sourceId, @sourceFileId)`)
       .run({
         date: file.date,
-        metadata: JSON.stringify(file.metadata),
         sourceFileId: file.sourceFileId,
         sourceId: file.sourceId,
         timestampAdded: new Date().getTime(),
@@ -55,7 +51,6 @@ function dbRecordToFile(dbRecord) {
   return new File({
     id: dbRecord.id,
     date: dbRecord.date,
-    metadata: JSON.parse(dbRecord.metadata),
     sourceFileId: dbRecord.source_file_id,
     sourceId: dbRecord.source_id,
   });
