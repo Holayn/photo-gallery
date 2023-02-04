adcf762514ee9592c60d51361ccaeb2e08bd6257
Holayn <kwwong1597@gmail.com>
2023-02-04 11:12:31 -0500

    fix(server): null checks.

--- packages/server/services/user.js
+++ packages/server/services/user.js
@@ -4,7 +4,9 @@ const User = require('../model/user');
 
 module.exports = {
   isValidUser(name, password) {
-    const hash = crypto.createHash('sha256').update(password).digest('hex');
-    return !!User.getByNamePass(name, hash);
+    if (name && password) {
+      const hash = crypto.createHash('sha256').update(password).digest('hex');
+      return !!User.getByNamePass(name, hash);
+    }
   },
 }
