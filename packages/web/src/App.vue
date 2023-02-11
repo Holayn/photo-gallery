66bfda9ded8d3220e1062adc7eaa9b71e6547f56
Holayn <kwwong1597@gmail.com>
2023-02-11 14:16:45 -0500

    feat(web): sticky header, move some controls to header.

--- packages/web/src/App.vue
+++ packages/web/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <div>
-      <header class="flex items-center px-4 md:px-8 py-4">
+      <header id="header" class="sticky top-0 bg-white z-40 shadow-md flex items-center gap-2 px-4 md:px-8 py-4">
         <div v-if="$store.state.isAdmin" class="flex-auto cursor-pointer">
           <svg @click="showSidebar = true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><line x1="3" y1="12" x2="21" y2="12"></line><line x1="3" y1="6" x2="21" y2="6"></line><line x1="3" y1="18" x2="21" y2="18"></line></svg>
         </div>
@@ -10,7 +10,7 @@
       <router-view></router-view>
     </div>
 
-    <nav v-if="showSidebar" class="absolute top-0 left-0 w-full h-full bg-black/25" @click="showSidebar = false">
+    <nav v-if="showSidebar" class="fixed z-50 top-0 left-0 w-full h-full bg-black/25" @click="showSidebar = false">
       <div class="bg-white px-8 py-4 h-full" style="width: 256px;" @click.stop>
         <h2 class="text-3xl">Photo Gallery</h2>
         <div class="mt-4 border-t border-slate-100"></div>
--- packages/web/src/views/Gallery.vue
+++ packages/web/src/views/Gallery.vue
@@ -4,21 +4,23 @@
       <div class="flex-auto break-all">
         <slot name="heading"></slot>
       </div>
-      <div v-if="$store.state.isAdmin" class="flex gap-4 justify-end pb-4">
-        <div v-if="isSelectionMode" class="flex gap-2 items-center">
-          <div>Selected: {{ Object.keys(selected).length }}</div>
-          <button class="px-2 py-1 bg-orange-100" :disabled="!Object.keys(selected).length" @click="showAlbumSelection()">Add to Existing Album</button>
-          <button class="px-2 py-1 bg-orange-100" :disabled="!Object.keys(selected).length" @click="createAlbumFromSelected()">Create Album</button>
-          <Loading v-if="loadingCreateAlbum"></Loading>
-          <button @click="toggleSelect()">
-            <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24" fill="none" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg>
+      <Teleport to="#header">
+        <div v-if="$store.state.isAdmin" class="flex gap-4 justify-end">
+          <div v-if="isSelectionMode" class="flex gap-2 items-center">
+            <div>Selected: {{ Object.keys(selected).length }}</div>
+            <button class="px-2 py-1 h-full bg-orange-100" :disabled="!Object.keys(selected).length" @click="showAlbumSelection()">Add to Existing Album</button>
+            <button class="px-2 py-1 h-full bg-orange-100" :disabled="!Object.keys(selected).length" @click="createAlbumFromSelected()">Create Album</button>
+            <Loading v-if="loadingCreateAlbum"></Loading>
+            <button @click="toggleSelect()">
+              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg>
+            </button>
+          </div>
+          <button v-else @click="toggleSelect()">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><polyline points="9 11 12 14 22 4"></polyline><path d="M21 12v7a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h11"></path></svg>
           </button>
+          <slot name="controls"></slot>
         </div>
-        <button v-else @click="toggleSelect()">
-          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><polyline points="9 11 12 14 22 4"></polyline><path d="M21 12v7a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h11"></path></svg>
-        </button>
-        <slot name="controls"></slot>
-      </div>
+      </Teleport>
     </div>
 
     <div id="media" class="justified-gallery">  
