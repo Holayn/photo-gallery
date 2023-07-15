425d335ce3e1f37ce6a1a9b3692f2572054dd2d3
Holayn <kwwong1597@gmail.com>
2023-07-15 18:44:58 -0400

    refactor(server): cleanup metadata.

--- packages/server/model/db-source-metadata.js
+++ packages/server/model/db-source-metadata.js
@@ -1,71 +1,56 @@
 const dayjs = require("dayjs");
-const customParseFormat = require("dayjs/plugin/customParseFormat");
+dayjs.extend(require("dayjs/plugin/customParseFormat"));
 
-dayjs.extend(customParseFormat);
-
-// mime type for videos
+const DATE_FORMAT = "YYYY:MM:DD HH:mm:ss";
 const MIME_VIDEO_REGEX = /^video\/.*$/;
-
-// standard EXIF date format, which is different from ISO8601
-const EXIF_DATE_FORMAT = "YYYY:MM:DD HH:mm:ss";
 
 class DbSourceMetadata {
   constructor(exif) {
-    this.date = getDate(exif);
+    this.date = date(exif);
     this.video = video(exif);
     const size = dimensions(exif);
     this.width = size.width;
     this.height = size.height;
-    this.orientation = tagValue(exif, "EXIF", "Orientation");
-    this.appleLivePhoto = !!tagValue(exif, "QuickTime", "LivePhotoAuto");
-    this.fileSize = tagValue(exif, "File", "FileSize");
-    this.fileName = tagValue(exif, "File", "FileName");
+    this.orientation = exif.EXIF?.Orientation;
+    this.appleLivePhoto = !!exif.QuickTime?.LivePhotoAuto;
+    this.fileSize = exif.File?.FileSize;
+    this.fileName = exif.File?.FileName;
     this.location = getLocation(exif);
     this.device = getDevice(exif);
   }
 }
 
-function getDate(exif) {
-  // first, check if there's a valid date in the metadata
-  const metadate = getMetaDate(exif);
-  if (metadate) return metadate.valueOf();
-  // otherwise, fallback to the last modified date
-  return dayjs(exif.File.FileModifyDate, EXIF_DATE_FORMAT).valueOf();
-}
-
-function getMetaDate(exif) {
-  const date =
-    tagValue(exif, "EXIF", "DateTimeOriginal") ||
-    tagValue(exif, "H264", "DateTimeOriginal") ||
-    tagValue(exif, "QuickTime", "ContentCreateDate") ||
-    tagValue(exif, "QuickTime", "CreationDate") ||
-    tagValue(exif, "QuickTime", "CreateDate");
-  if (date) {
-    const parsed = dayjs(date, EXIF_DATE_FORMAT);
-    if (parsed.isValid()) return parsed;
+function date(exif) {
+  const date = 
+    exif.EXIF?.DateTimeOriginal ||
+    exif.H264?.DateTimeOriginal ||
+    exif.QuickTime?.ContentCreateDate ||
+    exif.QuickTime?.CreationDate ||
+    exif.QuickTime?.CreateDate ||
+    exif.XMP?.CreateDate ||
+    exif.XMP?.DateCreated;
+  if (date && dayjs(date, DATE_FORMAT).isValid()) {
+    return dayjs(date, DATE_FORMAT).format(DATE_FORMAT);
+  } else {
+    return dayjs(exif.File.FileModifyDate, DATE_FORMAT).format(DATE_FORMAT);
   }
-  return null;
 }
 
 function getLocation(exif) {
-  const altitudeVal = tagValue(exif, "EXIF", "GPSAltitude");
+  const altitudeVal = exif.EXIF?.GPSAltitude;
   if (altitudeVal) {
     const [altNum, altUnit] = altitudeVal.split(" ");
-    const latRef = tagValue(exif, "EXIF", "GPSLatitudeRef");
-    const longRef = tagValue(exif, "EXIF", "GPSLongitudeRef");
-    const latVal = tagValue(exif, "EXIF", "GPSLatitude");
-    const longVal = tagValue(exif, "EXIF", "GPSLongitude");
+    const latRef = exif.EXIF?.GPSLatitudeRef;
+    const longRef = exif.EXIF?.GPSLongitudeRef;
+    const latVal = exif.EXIF?.GPSLatitude;
+    const longVal = exif.EXIF?.GPSLongitude;
     const lat = latRef === "North" ? latVal : -1 * latVal;
     const long = longRef === "East" ? latVal : -1 * longVal;
 
     return {
       lat,
       long,
-      altitude: `${Math.round(altNum)}${altUnit} ${tagValue(
-        exif,
-        "EXIF",
-        "GPSAltitudeRef"
-      )}`,
+      altitude: `${Math.round(altNum)}${altUnit} ${exif.EXIF?.GPSAlTitudeRef}`,
     };
   }
   return {
@@ -74,18 +59,11 @@ function getLocation(exif) {
 }
 
 function getDevice(exif) {
-  return (
-    tagValue(exif, "EXIF", "HostComputer") || tagValue(exif, "EXIF", "Model")
-  );
+  return exif.EXIF?.HostComputer || exif.EXIF?.Model;
 }
 
 function video(exif) {
-  return MIME_VIDEO_REGEX.test(exif.File.MIMEType);
-}
-
-function tagValue(exif, type, name) {
-  if (!exif[type]) return null;
-  return exif[type][name];
+  return MIME_VIDEO_REGEX.test(exif.File?.MIMEType);
 }
 
 function dimensions(exif) {
