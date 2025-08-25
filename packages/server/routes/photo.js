c81f1eb491ea23e432c1171a6cc80f6448d9f533
Holayn <kwwong1597@gmail.com>
2025-08-24 23:39:59 -0400

    Stream file data to fix memory issues.

--- packages/server/routes/photo.js
+++ packages/server/routes/photo.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
 
 const SourceService = require('../services/source');
 const AuthController = require('../controllers/auth');
@@ -24,7 +25,7 @@ router.get(
       const { path, fileType } = fileData;
       res.contentType(fileType);
       res.setHeader('Cache-Control', 'public, max-age=86400');
-      res.sendFile(path);
+      streamFile(req, res, path);
     } else {
       res.sendStatus(404);
     }
@@ -45,11 +46,48 @@ router.get(
         'Content-Disposition',
         `attachment; filename=${path.basename(p)}`
       );
-      res.sendFile(p);
+      streamFile(req, res, p);
     } else {
       res.sendStatus(404);
     }
   })
 );
+
+function streamFile(req, res, filePath) {
+  const fileType = path.extname(filePath);
+
+  if (fileType === 'mp4') {
+    const stat = fs.statSync(filePath);
+    const fileSize = stat.size;
+    const range = req.headers.range;
+
+    if (range) {
+      const parts = range.replace(/bytes=/, "").split("-");
+      const start = parseInt(parts[0], 10);
+      const end = parts[1] ? parseInt(parts[1], 10) : fileSize - 1;
+
+      const chunksize = (end - start) + 1;
+      const file = fs.createReadStream(filePath, { start, end });
+      const head = {
+        'Content-Range': `bytes ${start}-${end}/${fileSize}`,
+        'Accept-Ranges': 'bytes',
+        'Content-Length': chunksize,
+        'Content-Type': 'video/mp4',
+      };
+
+      res.writeHead(206, head);
+      file.pipe(res);
+    } else {
+      const head = {
+        'Content-Length': fileSize,
+        'Content-Type': 'video/mp4',
+      };
+      res.writeHead(200, head);
+      fs.createReadStream(filePath).pipe(res);
+    }
+  } else {
+    fs.createReadStream(filePath).pipe(res);
+  }
+}
 
 module.exports = router;
