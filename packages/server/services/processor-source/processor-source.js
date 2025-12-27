1e530081bf9435180dc5cf21905cf63c16dcfa8c
Holayn <kwwong1597@gmail.com>
2025-12-27 17:22:26 -0500

    Update index read logic to handle new updates in processor index.

--- packages/server/services/processor-source/processor-source.js
+++ packages/server/services/processor-source/processor-source.js
@@ -8,13 +8,20 @@ const { toModelFactory } = require('../../util/db-utils');
 
 const SOURCE_INDEX_DB_FILENAME = 'index.db';
 const FILES_TABLE_NAME = 'files';
-
-const SIZE_COLUMNS = {
-  LARGE: 'processed_path_large',
-  ORIGINAL: 'processed_path_original',
-  SMALL: 'processed_path_small',
-  THUMB: 'processed_path_thumb',
-};
+const SOURCE_FILE_PATH_PREFIX = 'media';
+const SIZE_LARGE = 'large';
+const SIZE_SMALL = 'small';
+const SIZE_THUMB = 'thumb';
+const SIZE_ORIGINAL = 'original';
+const VALID_SIZES = [SIZE_LARGE, SIZE_SMALL, SIZE_THUMB, SIZE_ORIGINAL];
+const IMG_EXTS_NEED_CONVERSION = ['.heic'];
+const VIDEO_EXTS_NEED_CONVERSION = ['.mov'];
+const CONVERTED_DIR = 'converted';
+const CONVERTED_IMG_SUFFIX = '__.jpg';
+const CONVERTED_VIDEO_SUFFIX = '__.mp4';
+const HDR_VIDEO_PREVIEW_SUFFIX = '__.png';
+const SDR_VIDEO_PREVIEW_SUFFIX = '__.jpg';
+const VIDEO_PREVIEW_SIZES = [SIZE_SMALL, SIZE_THUMB];
 
 const connections = {};
 
@@ -31,7 +38,7 @@ class ProcessorSource {
       if (!connections[sourcePath]) {
         logger.info(`Opening photo-web-processor source: ${sourcePath}`);
         const sourceIndexDb = new Database(
-          path.resolve(sourcePath, SOURCE_INDEX_DB_FILENAME)
+          path.join(sourcePath, SOURCE_INDEX_DB_FILENAME)
         );
         this.db = sourceIndexDb;
         connections[sourcePath] = sourceIndexDb;
@@ -43,12 +50,7 @@ class ProcessorSource {
 
   getDirectories() {
     const records = this.db
-      .prepare(
-        `
-        SELECT path FROM ${FILES_TABLE_NAME} 
-        WHERE processed != 0
-      `
-      )
+      .prepare(`SELECT path FROM ${FILES_TABLE_NAME} WHERE processed != 0`)
       .all();
     const paths = new Set();
     records.forEach((r) => {
@@ -109,7 +111,7 @@ class ProcessorSource {
     return files.map((f) => toModel(f));
   }
 
-  async getFileData(id, size) {
+  async getFileData(id, sizeParam) {
     const fileRecord = this.db
       .prepare(`SELECT * FROM ${FILES_TABLE_NAME} WHERE id = ?`)
       .get(id);
@@ -118,27 +120,15 @@ class ProcessorSource {
       throw new Error(`ID:${id} does not exist in processor ${this.path}.`);
     }
 
-    const pathColumn = SIZE_COLUMNS[size.toUpperCase()];
-    if (!pathColumn) {
+    const size = sizeParam.toLowerCase();
+
+    if (!VALID_SIZES.includes(size)) {
       throw new Error('Invalid size');
     }
 
-    const sourcePath = fileRecord[pathColumn];
-    if (!sourcePath) {
-      throw new Error(`${pathColumn} does not exist for ${id}`);
-    }
-
     // Attempt to return the original file's web-converted copy.
-    if (SIZE_COLUMNS[size.toUpperCase()] === SIZE_COLUMNS.ORIGINAL) {
-      const largePath = fileRecord[SIZE_COLUMNS.LARGE];
-      if (!largePath) {
-        throw new Error('Failed to derive path for converted file.');
-      }
-      const convertedPath = fileRecord[SIZE_COLUMNS.LARGE].replace(
-        'large',
-        'converted'
-      );
-      const photoPath = path.resolve(this.path, convertedPath);
+    if (size === SIZE_ORIGINAL) {
+      const photoPath = path.join(this.path, SOURCE_FILE_PATH_PREFIX, CONVERTED_DIR, fileRecord.path);
       const exists = await fs.pathExists(photoPath);
       if (exists) {
         return {
@@ -148,17 +138,52 @@ class ProcessorSource {
       }
     }
 
-    const photoPath = path.resolve(this.path, sourcePath);
-    const exists = await fs.pathExists(photoPath);
+    const get = async sourcePath => {
+      let photoPath = path.join(this.path, sourcePath);
 
-    if (exists) {
-      return {
-        path: photoPath,
-        fileType: path.extname(photoPath),
-      };
+      const ext = path.extname(sourcePath);
+      if (IMG_EXTS_NEED_CONVERSION.includes(ext.toLowerCase())) {
+        photoPath += CONVERTED_IMG_SUFFIX;
+      } else if (VIDEO_EXTS_NEED_CONVERSION.includes(ext.toLowerCase())) {
+        photoPath += CONVERTED_VIDEO_SUFFIX;
+      }
+
+      const exists = await fs.pathExists(photoPath);
+
+      if (exists) {
+        return {
+          path: photoPath,
+          fileType: path.extname(photoPath),
+        };
+      }
+
+      return null;
+    };
+
+    const metadata = fileRecord.metadata ? JSON.parse(fileRecord.metadata) : {};
+    const video = metadata.File.MIMEType?.includes('video') ?? false;
+
+    if (video) {
+      if (VIDEO_PREVIEW_SIZES.includes(size)) {
+        const hdr = metadata.WebImg?.HDR ?? false;
+        if (hdr) {
+          const result = await get(path.join(SOURCE_FILE_PATH_PREFIX, size, fileRecord.path + HDR_VIDEO_PREVIEW_SUFFIX));
+          if (result) {
+            return result;
+          } else {
+            // Fallback to SDR preview if HDR preview does not exist.
+            return get(path.join(SOURCE_FILE_PATH_PREFIX, size, fileRecord.path + SDR_VIDEO_PREVIEW_SUFFIX));
+          }
+        } else {
+          return get(path.join(SOURCE_FILE_PATH_PREFIX, size, fileRecord.path + SDR_VIDEO_PREVIEW_SUFFIX));
+        }
+      } else {
+        return get(path.join(SOURCE_FILE_PATH_PREFIX, size, fileRecord.path));
+      }
+      
+    } else {
+      return get(path.join(SOURCE_FILE_PATH_PREFIX, size, fileRecord.path));
     }
-
-    return null;
   }
 
   async getFilePath(id) {
@@ -170,22 +195,7 @@ class ProcessorSource {
       throw new Error(`ID:${id} does not exist in processor ${this.path}.`);
     }
 
-    const sourcePath = fileRecord[SIZE_COLUMNS.ORIGINAL];
-    if (!sourcePath) {
-      throw new Error(`${SIZE_COLUMNS.ORIGINAL} does not exist for ${id}`);
-    }
-
-    // Attempt to return the original file's web-converted copy.
-    const convertedPath = fileRecord[SIZE_COLUMNS.ORIGINAL].replace(
-      'original',
-      'converted'
-    );
-    const photoPath = path.resolve(this.path, convertedPath);
-    const exists = await fs.pathExists(photoPath);
-    if (exists) {
-      return photoPath;
-    }
-    return path.resolve(this.path, fileRecord[SIZE_COLUMNS.ORIGINAL]);
+    return path.join(this.path, SOURCE_FILE_PATH_PREFIX, SIZE_ORIGINAL, fileRecord.path);
   }
 }
 
