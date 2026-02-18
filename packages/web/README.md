1f75c7dc3effcc0112c4c5710179ea957b2eaaa9
Holayn <kwwong1597@gmail.com>
2026-02-18 15:23:30 -0500

    Unused.

--- packages/web/README.md
+++ packages/web/README.md
@@ -1,8 +1,5 @@
 # photo-gallery-web
 
-## Setup
-- Define `VUE_APP_BASE_URL` in `.env` - this should be the base url the app is being hosted at (e.g. /photos)
-
 ## Startup
 
 ### Development
--- packages/web/vite.config.js
+++ packages/web/vite.config.js
@@ -1,9 +1,8 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '')
   return {
     build: {
       sourcemap: true,
@@ -16,9 +15,7 @@ export default defineConfig(({ mode }) => {
         }
       }
     })],
-    base: mode === 'production'
-    ? env.VUE_APP_BASE_URL
-    : '/',
+    base: '/',
     server: {
       proxy: {
         '/api': {
