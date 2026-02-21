a887dce6238eeb74dd6c89f9ddc0e010c84858ae
Holayn <kwwong1597@gmail.com>
2026-02-20 22:21:52 -0500

    Use kaiauth package.

--- packages/auth/index.js
+++
@@ -1,7 +0,0 @@
-const createAuthRouter = require('./lib/auth-router');
-const UserStore = require('./lib/user-store');
-
-module.exports = {
-  createAuthRouter,
-  UserStore,
-};
--- packages/auth/lib/utils.js
+++
@@ -1,9 +0,0 @@
-function timingSafeCompare(a, b) {
-  const bufA = Buffer.from(a);
-  const bufB = Buffer.from(b);
-  return bufA.length === bufB.length && crypto.timingSafeEqual(bufA, bufB);
-}
-
-module.exports = {
-  timingSafeCompare,
-};
