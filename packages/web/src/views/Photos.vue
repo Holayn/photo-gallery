6a3266bf2210d93424ccf46d385552827bf803a1
Holayn <kwwong1597@gmail.com>
2026-09-12 00:36:57 -0400

    Make photo of day use larger size when possible

--- packages/web/src/views/Photos.vue
+++ packages/web/src/views/Photos.vue
@@ -34,7 +34,8 @@
     <div v-else-if="!photoOfDay">No photo of the day</div>
     <div v-else class="w-32 md:w-60">
       <button class="w-full p-1 bg-slate-100 rounded-md" style="aspect-ratio: 1/1;" @click="showPhotoOfDay = true">
-        <img class="w-full h-full object-cover rounded-sm" :src="photoOfDay.urls.view[PHOTO_SIZES.SMALL]">
+        <img class="hidden md:block w-full h-full object-cover rounded-sm" :src="photoOfDay.urls.view[photoOfDay.metadata.video ? PHOTO_SIZES.SMALL : PHOTO_SIZES.LARGE]">
+        <img class="md:hidden w-full h-full object-cover rounded-sm" :src="photoOfDay.urls.view[PHOTO_SIZES.SMALL]">
       </button>
     </div>
 
